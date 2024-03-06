688d418e748226a408d9c121de1bb2bd143d5c2f
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-03-06 00:51:32 -0300

    adicionando a tela de detalhes de filme

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,7 +1,63 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import './style.css'
+
+import { api } from '../../services/api'
+
 export function Filme() {
+    const { id } = useParams()
+    const [filme, setFilme] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: 'c70192348765194da64b6af0596492f2',
+                    language: 'pt-BR',
+                }
+            })
+            .then((response) => {
+                setFilme(response.data)
+                setLoading(false);
+            })
+            .catch(() => {
+                console.log('filme não encontrado!')
+            })
+        }
+        loadFilme()
+
+        return () => {
+            console.log('Componente foi desmontado!')
+        }
+    }, [])
+
+    if (loading) {
+        return (
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h1>Filmes</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">
+                        Trailer
+                    </a>
+                </button>
+            </div>
+            
         </div>
     )
 }
