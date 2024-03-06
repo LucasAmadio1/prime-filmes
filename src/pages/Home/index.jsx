955ae48ecc8836064e00e83cfd182a83a6fd35b1
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-03-05 23:53:30 -0300

    aula 3

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
 import { api } from "../../services/api"
+import { Link } from "react-router-dom"
+import './style.css'
 
 // URL DA API: https://api.themoviedb.org/3/movie/now_playing?api_key=c70192348765194da64b6af0596492f2&language=pt-BR
 
 export function Home() {
     const [movies, setMovies] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         async function loadFilmes() {
@@ -16,15 +19,34 @@ export function Home() {
                 }
             })
 
-            console.log(response.data.results)
+            setMovies(response.data.results.slice(0, 10))
+            setLoading(false)
         }
 
         loadFilmes()    
-    }, [])
+    }, []) 
+
+    if (loading) {
+        return (
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
 
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="container">
+            <div className="list-movies">
+                {movies.map((movie) => {
+                    return (
+                        <article key={movie.id}>
+                            <strong>{movie.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+                            <Link to={`/filme/${movie.id}`} >Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
